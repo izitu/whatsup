037b71608ba571771595f772c474cab0b2aedcdd
Andrew Ivanov <nik1297@gmail.com>
2021-01-15 15:56:24 +0300

    4

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
-# This is a sample Python script.
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import time
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+contact = "Женюньчик"
+text = "Hey, this message was sent using Selenium"
+driver = webdriver.Chrome()
+driver.get("https://web.whatsapp.com")
+print("Scan QR Code, And then Enter")
+input()
+print("Logged In")
+inp_xpath_search = "//*[@id=\"side\"]/div[1]/div/label/div/div[2]"
+input_box_search = WebDriverWait(driver,50).until(lambda driver: driver.find_element_by_xpath(inp_xpath_search))
+input_box_search.click()
+time.sleep(2)
+input_box_search.send_keys(contact)
+time.sleep(2)
+selected_contact = driver.find_element_by_xpath("//*[@id=\"pane-side\"]/div[1]/div/div/div[19]/div/div/div[1]/div/img")
+selected_contact.click()
+inp_xpath = '//*[@id="main"]/footer/div[1]/div[2]'
+input_box = driver.find_element_by_xpath(inp_xpath)
+time.sleep(2)
+input_box.send_keys(text + Keys.ENTER)
+time.sleep(2)
+driver.quit()
